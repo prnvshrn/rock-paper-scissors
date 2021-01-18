f3672bb88791dc1776ff0493de1133eb5f4de192
prnvshrn <prnvshrn@gmail.com>
2021-01-18 00:20:11 -0800

    Setting up project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import logo from './rock-paper-scissor.jpg';
 import './App.css';
+import rock_icon from './rock-icon.png';
+import paper_icon from './paper-icon.png';
+import scissor_icon from './scissor-icon.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="container">
+          <div class="row">
+            <div class="col-sm-4">
+              <img src={logo} class="main-logo" alt="logo" />
+            </div>
+            <div class="col-sm-8">
+             <h1>
+              Rock Paper Scissors
+              </h1>
+            </div>
+          </div>
+        </div>
+      <div class="container">
+          <div class="row">
+            <div class="col-sm-4">Rock</div>
+            <div class="col-sm-4">Paper</div>
+            <div class="col-sm-4">Scissors</div>
+          </div>
+      </div>
     </div>
   );
 }
